5b922543b7a7dccd78599d066417d656fc91734f
Iagho Slaviero <iaghohsc1@gmail.com>
2023-12-04 11:58:31 -0300

    #

--- src/telaCadastro/TelaUsuario.jsx
+++ src/telaCadastro/TelaUsuario.jsx
@@ -2,7 +2,6 @@ import { Container } from "react-bootstrap";
 import CadastroUsuario from "./CadastroUsuario";
 import TabelaUsuario from "./TabelaUsuario";
 import Pagina from "../templates/Pagina";
-import { useState } from "react";
 
 export default function TelaUsuario(props) {
 
